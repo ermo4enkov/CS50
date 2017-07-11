413f9d1b2dc2c46071e5ea77541bf57840af7ddf
ermo4enkov <ermo4enkov@gmail.com>
2017-07-12 02:04:18 +0300

    Created return my own function in c

---
+++ week_1/return.c
@@ -0,0 +1,15 @@
+#include <cs50.h>
+#include <stdio.h>
+int getSquare(int x);
+int main(void)
+{
+    printf("X is: ");
+    int x = get_int();
+
+    printf("X^X is: %i \n", getSquare(x));
+}
+
+int getSquare(int x)
+{
+    return x * x;
+}
