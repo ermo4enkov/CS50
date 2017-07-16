c149c9dece08bf6be1fba929bde8c90033f28e77
ermo4enkov <ermo4enkov@gmail.com>
2017-07-16 21:50:23 +0300

    Adde caesar.c

---
+++ week2/pset2/caesar.c
@@ -0,0 +1,53 @@
+#include <cs50.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(int argc, string argv[])
+{
+
+    if (argc != 2)
+    {
+        printf("missing command-line argument\n");
+        return 1;
+    }
+
+    int gap = atoi(argv[1]);
+
+    printf("plaintext: ");
+
+    string start = get_string();
+
+    int wrap = gap % 26;
+
+    printf("ciphertext: ");
+    for(int i = 0, len = strlen(start); i < len; i++)
+    {
+
+        if (gap < 27)
+        {
+            // проверка на то, является ли символ после добавления буквой
+            if (isalpha(start[i] = start[i] + gap))
+            {
+                printf("%c", start[i] = start[i] + gap-1);
+                // печатаю полученный символ
+            } else
+            {
+                // если деление по модулю равно нулю, значит просто переходим круг букв
+                printf("%c", start[i] = (start[i] - 27) + gap);
+            }
+        }
+        else
+        {
+            if (wrap > 0 )
+            {
+                printf("%c", start[i] = ((start[i] - 27)-wrap) + gap);
+            }
+        }
+    }
+    return 0;
+}
+
+// если деление по модулю на 26 больше нулю, то
+            //
